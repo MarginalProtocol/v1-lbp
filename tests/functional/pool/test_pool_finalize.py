095dcee25703412b214bd4bbad0c18bc4326fb8f
0xcivita <0xcivita@proton.me>
2024-08-24 11:40:12 -0400

    test pool finalize revert cases

--- tests/functional/pool/test_pool_finalize.py
+++ tests/functional/pool/test_pool_finalize.py
@@ -1,6 +1,9 @@
 import pytest
 
-from utils.constants import MIN_SQRT_RATIO, MAX_SQRT_RATIO
+from hexbytes import HexBytes
+from ape import reverts
+
+from utils.constants import MIN_SQRT_RATIO, MAX_SQRT_RATIO, MINIMUM_DURATION
 from utils.utils import calc_range_amounts_from_liquidity_sqrt_price_x96
 
 
@@ -209,13 +212,242 @@ def test_pool_finalize__transfers_funds(
     assert token1.balanceOf(alice.address) == balance1_alice
 
 
-def test_pool_finalize__returns_amounts():
-    pass
+@pytest.mark.parametrize("fee_protocol", [0, 10, 100])
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__returns_amounts(
+    pool_finalized,
+    factory,
+    callee,
+    sender,
+    admin,
+    alice,
+    token0,
+    token1,
+    range_math_lib,
+    chain,
+    fee_protocol,
+    init_with_sqrt_price_lower_x96,
+):
+    factory.setFeeProtocol(fee_protocol, sender=admin)
+    pool_finalized_with_liquidity = pool_finalized(init_with_sqrt_price_lower_x96)
+    state = pool_finalized_with_liquidity.state()
+    assert state.feeProtocol == fee_protocol
+    assert state.finalized is True
+
+    shares = pool_finalized_with_liquidity.balanceOf(
+        pool_finalized_with_liquidity.address
+    )
+    total_supply = pool_finalized_with_liquidity.totalSupply()
+    assert shares > 0
+    assert total_supply > 0
+
+    # calc amounts in pool
+    sqrt_price_lower_x96 = pool_finalized_with_liquidity.sqrtPriceLowerX96()
+    sqrt_price_upper_x96 = pool_finalized_with_liquidity.sqrtPriceUpperX96()
+    (amount0, amount1) = calc_range_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity,
+        state.sqrtPriceX96,
+        sqrt_price_lower_x96,
+        sqrt_price_upper_x96,
+    )
+
+    tx = callee.finalize(
+        pool_finalized_with_liquidity.address,
+        alice.address,
+        sender=sender,
+    )
+    return_log = tx.decode_logs(callee.FinalizeReturn)[0]
+    assert return_log.liquidityDelta == state.liquidity
+    assert return_log.sqrtPriceX96 == state.sqrtPriceX96
+    assert return_log.amount0 == amount0
+    assert return_log.amount1 == amount1
 
 
-def test_pool_finalize__emits_finalize():
-    pass
+@pytest.mark.parametrize("fee_protocol", [0, 10, 100])
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__emits_finalize(
+    pool_finalized,
+    factory,
+    callee,
+    sender,
+    admin,
+    alice,
+    token0,
+    token1,
+    range_math_lib,
+    chain,
+    fee_protocol,
+    init_with_sqrt_price_lower_x96,
+):
+    factory.setFeeProtocol(fee_protocol, sender=admin)
+    pool_finalized_with_liquidity = pool_finalized(init_with_sqrt_price_lower_x96)
+    state = pool_finalized_with_liquidity.state()
+    assert state.feeProtocol == fee_protocol
+    assert state.finalized is True
+
+    shares = pool_finalized_with_liquidity.balanceOf(
+        pool_finalized_with_liquidity.address
+    )
+    total_supply = pool_finalized_with_liquidity.totalSupply()
+    assert shares > 0
+    assert total_supply > 0
+
+    # calc amounts in pool
+    sqrt_price_lower_x96 = pool_finalized_with_liquidity.sqrtPriceLowerX96()
+    sqrt_price_upper_x96 = pool_finalized_with_liquidity.sqrtPriceUpperX96()
+    (amount0, amount1) = calc_range_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity,
+        state.sqrtPriceX96,
+        sqrt_price_lower_x96,
+        sqrt_price_upper_x96,
+    )
+
+    tx = callee.finalize(
+        pool_finalized_with_liquidity.address,
+        alice.address,
+        sender=sender,
+    )
+    events = tx.decode_logs(pool_finalized_with_liquidity.Finalize)
+    assert len(events) == 1
+    event = events[0]
+
+    assert event.liquidityDelta == state.liquidity
+    assert event.sqrtPriceX96 == state.sqrtPriceX96
+    assert event.amount0 == amount0
+    assert event.amount1 == amount1
 
 
-def test_pool_finalize__updates_state_when_exit():
-    pass
+@pytest.mark.parametrize("fee_protocol", [0, 10, 100])
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__updates_state_when_exit(
+    factory,
+    pool_initialized,
+    callee,
+    sender,
+    admin,
+    alice,
+    chain,
+    fee_protocol,
+    init_with_sqrt_price_lower_x96,
+):
+    factory.setFeeProtocol(fee_protocol, sender=admin)
+    pool_initialized_with_liquidity = pool_initialized(init_with_sqrt_price_lower_x96)
+    state = pool_initialized_with_liquidity.state()
+    assert state.feeProtocol == fee_protocol
+    assert state.finalized is False
+
+    timestamp_initialize = pool_initialized_with_liquidity.blockTimestampInitialize()
+    chain.mine(timestamp=timestamp_initialize + MINIMUM_DURATION + 1)
+
+    # update liquidity for burn
+    state.liquidity = 0
+
+    # state should toggle to finalized
+    state.finalized = True
+
+    # update oracle write
+    block_timestamp_next = chain.pending_timestamp
+    tick_cumulative_next = state.tickCumulative + state.tick * (
+        block_timestamp_next - state.blockTimestamp
+    )
+    state.tickCumulative = tick_cumulative_next
+    state.blockTimestamp = block_timestamp_next
+
+    callee.finalize(
+        pool_initialized_with_liquidity.address, alice.address, sender=sender
+    )
+
+    assert pool_initialized_with_liquidity.state() == state
+
+
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__reverts_when_not_supplier(
+    pool_finalized,
+    factory,
+    callee,
+    sender,
+    admin,
+    alice,
+    token0,
+    token1,
+    range_math_lib,
+    chain,
+    init_with_sqrt_price_lower_x96,
+):
+    pool_finalized_with_liquidity = pool_finalized(init_with_sqrt_price_lower_x96)
+    state = pool_finalized_with_liquidity.state()
+    assert state.finalized is True
+    assert pool_finalized_with_liquidity.supplier() != sender.address
+
+    with reverts(pool_finalized_with_liquidity.Unauthorized):
+        data = HexBytes("")
+        pool_finalized_with_liquidity.finalize(data, sender=sender)
+
+
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__reverts_when_not_finalized(
+    pool_initialized,
+    callee,
+    sender,
+    admin,
+    alice,
+    chain,
+    init_with_sqrt_price_lower_x96,
+):
+    pool_initialized_with_liquidity = pool_initialized(init_with_sqrt_price_lower_x96)
+    state = pool_initialized_with_liquidity.state()
+    assert state.finalized is False
+
+    with reverts(pool_initialized_with_liquidity.NotFinalized):
+        callee.finalize(
+            pool_initialized_with_liquidity.address, alice.address, sender=sender
+        )
+
+
+@pytest.mark.parametrize("init_with_sqrt_price_lower_x96", [True, False])
+def test_pool_finalize__reverts_when_no_supply(
+    pool_finalized,
+    factory,
+    callee,
+    sender,
+    admin,
+    alice,
+    token0,
+    token1,
+    range_math_lib,
+    chain,
+    init_with_sqrt_price_lower_x96,
+):
+    # finalize first then try again
+    pool_finalized_with_liquidity = pool_finalized(init_with_sqrt_price_lower_x96)
+    state = pool_finalized_with_liquidity.state()
+    assert state.finalized is True
+
+    shares = pool_finalized_with_liquidity.balanceOf(
+        pool_finalized_with_liquidity.address
+    )
+    total_supply = pool_finalized_with_liquidity.totalSupply()
+    assert shares > 0
+    assert total_supply > 0
+
+    # calc amounts in pool
+    sqrt_price_lower_x96 = pool_finalized_with_liquidity.sqrtPriceLowerX96()
+    sqrt_price_upper_x96 = pool_finalized_with_liquidity.sqrtPriceUpperX96()
+    (amount0, amount1) = calc_range_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity,
+        state.sqrtPriceX96,
+        sqrt_price_lower_x96,
+        sqrt_price_upper_x96,
+    )
+
+    callee.finalize(
+        pool_finalized_with_liquidity.address,
+        alice.address,
+        sender=sender,
+    )
+    assert pool_finalized_with_liquidity.totalSupply() == 0
+
+    with reverts(pool_finalized_with_liquidity.SupplyLessThanMin):
+        callee.finalize(
+            pool_finalized_with_liquidity.address, alice.address, sender=sender
+        )
